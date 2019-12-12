40ec6f6575d00314cfce6abe42105ede0f6ed43f
harri-hakola <harri.hakola@seamk.fi>
2019-12-12 21:47:13 +0200

    Hello world lisäys

---
+++ HelloTesti/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.HelloTesti
+{
+    public class Program
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
